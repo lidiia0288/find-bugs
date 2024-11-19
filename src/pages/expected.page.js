8a51821dd139f3d48d073e349c13720c34044a66
Lida <l.karpova@freshauto2.ru>
2024-11-19 15:41:56 +0300

    commit tests

---
+++ src/pages/expected.page.js
@@ -0,0 +1,18 @@
+import { expect } from '@playwright/test';
+import * as allure from "allure-js-commons";
+import { BasePage } from "./base.page";
+
+export class ExpectedPage extends BasePage {    
+    constructor(page) {
+        super(page);
+        this.resultFrame = this.page.locator('#popmake-4406');
+    }
+
+    async goToExpect () {
+        await allure.step ("На главной странице отобразилась информация о найденном баге", async () => {
+        await expect(this.resultFrame).toBeVisible({ timeout: 80000 });
+        await expect(this.resultFrame).toContainText('In this bug', { timeout: 80000 });
+    });
+    }
+
+}
